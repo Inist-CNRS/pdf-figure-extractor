896194213239bfff2db0bef8495413a405a5ba98
labroche <corentin.labroche@inist.fr>
2017-04-19 17:05:42 +0200

    Transform code into class to be execute as module without conf file

--- openCVHocr.js
+++ openCVHocr.js
@@ -1,52 +1,66 @@
 const AreaHocr = require('./modules/hocrArea')
 const exec = require('child_process').exec
-const options = require('./conf.json')
 const bluebird = require('bluebird')
 const easyimg = require('easyimage')
 const fs = bluebird.promisifyAll(require('fs'))
 
 
-const filename = options.source.replace(/^.*[\\\/]/, '').replace(/\.[^/.]+$/, "")
-const outputFile = options.destinationDirectory + '/' + filename + '.txt'
-const hocrFilePath = options.destinationDirectory + '/' + filename + '.hocr'
+
+
+class openCVHocr {
+  constructor(options) {
+    this.options = options
+    this.filename = options.source.replace(/^.*[\\\/]/, '').replace(/\.[^/.]+$/, "")
+    this.arrayOfArea = new AreaHocr(options.destinationDirectory + '/' + this.filename + '.hocr').area()
+    this.outputFile = options.destinationDirectory + '/' + this.filename + '.txt'
+
+    if (fs.existsSync(this.outputFile)) {
+      fs.unlinkSync(this.outputFile);
+    }
+  }
+
+  exec() {
+    return bluebird.map(this.arrayOfArea, (area, index) => {
+      return this.analyseImage(index, area) // analyse des images sous forme asynchrone
+    }, {
+      concurrency: 30
+    }).then((values) => {
+      return values.sort((curr, next) => curr.i - next.i)
+    }).map((value) => {
+      return fs.appendFileAsync(this.outputFile, value.stdout)
+    }).then(_ => {
+      console.timeEnd("execution");
+    })
+  }
+
+  analyseImage(i, area) {
+    return new Promise((resolve, reject) => {
+      const options = this.options
+      easyimg.crop({
+        src: options.source,
+        dst: options.tmpDirectory + '/part' + i + '.png',
+        cropwidth: +area.width,
+        cropheight: +area.height,
+        x: +area.x1,
+        y: +area.y1
+      }).then((image) => {
+        exec('tesseract ' + options.tmpDirectory + '/part' + i + '.png stdout', (error, stdout, stderr) => {
+          if (error) reject(error)
+          else resolve({
+            i,
+            stdout
+          })
+        }, err => console.log(err))
+      }).catch(err => console.log(err))
+    })
+  }
+}
 
 console.time("execution");
-if (fs.existsSync(outputFile)) {
-  fs.unlinkSync(outputFile);
-}
+new openCVHocr({
+  "source": "images/test1.png",
+  "destinationDirectory": "output",
+  "tmpDirectory": "tmp"
+}).exec()
 
-const hocr = new AreaHocr(hocrFilePath)
-const arrayOfArea = hocr.area() // on recupere les areas dans le DOM
-return bluebird.map(arrayOfArea, (area, index) => {
-  return analyseImage(index, area) // analyse des images sous forme asynchrone
-}, {
-  concurrency: 30
-}).then((values) => {
-  return values.sort((curr, next) => curr.i - next.i)
-}).map((value) => {
-  return fs.appendFileAsync(outputFile, value.stdout)
-}).then(_ => {
-  console.timeEnd("execution");
-})
-
-
-function analyseImage(i, area) {
-  return new Promise((resolve, reject) => {
-    easyimg.crop({
-      src: options.source,
-      dst: options.tmpDirectory + '/part' + i + '.png',
-      cropwidth: +area.width,
-      cropheight: +area.height,
-      x: +area.x1,
-      y: +area.y1
-    }).then((image) => {
-      exec('tesseract ' + options.tmpDirectory + '/part' + i + '.png stdout', (error, stdout, stderr) => {
-        if (error) reject(error)
-        else resolve({
-          i,
-          stdout
-        })
-      }, err => console.log(err))
-    }).catch(err => console.log(err))
-  })
-}
+module.exports = openCVHocr
