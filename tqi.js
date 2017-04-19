2ede12a61c7b45b769136af1168fe83aee061e37
labroche <corentin.labroche@inist.fr>
2017-04-19 17:41:02 +0200

    adapt other script

--- tqi.js
+++ tqi.js
@@ -1,12 +1,22 @@
 const Tqi = require('text-quality-indicator'),
     tqi = new Tqi();
+const openCVHocr = require('./openCVHocr')
 const fs = require('fs');
 const exec = require('child_process').exec
 
-const options = require('./conf.json')
+const options = {
+  "source": "images/test1.png",
+  "destinationDirectory": "output",
+  "tmpDirectory": "tmp"
+}
 const filename = options.source.replace(/^.*[\\\/]/, '').replace(/\.[^/.]+$/, "")
 const cropFile = options.destinationDirectory + '/' + filename + '.txt'
 const tesseractFile = 'tmp/tesseract.txt'
+
+new openCVHocr(options).exec().then((result) => {
+  console.timeEnd("execution")
+})
+
 
 exec('tesseract ' + options.source + ' tmp/tesseract', (error, stdout, stderr) => {
   tqi.analyze(tesseractFile).then((result) => {
