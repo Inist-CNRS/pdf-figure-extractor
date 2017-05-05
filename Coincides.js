bc089273dd45cf0f4e156bc79884d84711f7fc26
labroche <corentin.labroche@inist.fr>
2017-05-05 15:53:13 +0200

    effet de bord

--- Coincides.js
+++ Coincides.js
@@ -12,6 +12,7 @@ const util = require('util')
 let Coincides = {}
 
 Coincides.init = (config) => {
+  Coincides.z='lk'
   return new Promise(function(resolve, reject) {
     if(!config.imageInputPath || !config.directoryOutputPath){
       reject("Un parametre de configuration est manquant")
@@ -20,6 +21,7 @@ Coincides.init = (config) => {
     for (var prop in config) {
       Coincides[prop] = config[prop]
     }
+    console.log('sjdflsdfhsdfjklh');
     Coincides.outputWithoutArray = `${config.directoryOutputPath}/${helpers.getFilename(config.imageInputPath)}/output.png`
     Coincides.directoryPartialPath = `${config.directoryOutputPath}/${helpers.getFilename(config.imageInputPath)}/partials`
     if (!fs.existsSync(Coincides.imageInputPath)){
@@ -27,14 +29,14 @@ Coincides.init = (config) => {
       return;
     }
     mkdirp.mkdirpAsync(`${config.directoryOutputPath}/${helpers.getFilename(config.imageInputPath)}/partials`).then(_=>{
-      resolve()
+      resolve(Coincides)
     }).catch(err=>reject(err))
   })
 }
 
 Coincides.exec = () => {
   bluebird.join(tesseract.init(Coincides.imageInputPath, [0,3]), opencv.init(Coincides.imageInputPath), function(tesseract, openCV) {
-    arrayOfOpenCV = openCV.filter().contours().get()
+    arrayOfOpenCV = []
     arrayOfTesseract = tesseract.getArray()
     const common = Coincides.compare(arrayOfTesseract, arrayOfOpenCV)
     helpers.writeOnImage(Coincides.imageInputPath, Coincides.outputWithoutArray, common)
@@ -75,4 +77,4 @@ Coincides.howManyRectangleInside = function (opencvTab, item) {
   return nbRect
 }
 
-module.exports = Coincides
+module.exports = Object.assign({}, Coincides);
