f1f131f781065a3fffa2398af047cfd0aa473194
labroche <corentin.labroche@inist.fr>
2017-05-30 18:28:18 +0200

    begin tests

--- test/tests.js
+++ test/tests.js
@@ -1,22 +1,27 @@
-const Coincides = require('../Coincides')
+
 const assert = require('chai').assert
 const exec = require('child_process').exec
 const expect = require('chai').expect
 const fs = require('fs');
+const pfe = require('../index.js')
+const Promise = require('bluebird')
+const path = require('path')
 
 
 const goodConfig = {
-  imageInputPath: 'test/images/test1.png',
-  imageOutputPath: 'test/output/test1.png',
-  hocrPath: 'test/hocr/test1.hocr'
+  pdfInputPath: path.join(__dirname, './pdf'),
+  directoryOutputPath: path.join(__dirname, './tmp/images'),
+  directoryPartialPath: path.join(__dirname, './tmp/images'),
+  tmp: path.join(__dirname, "./tmp"),
+  debug:true
 }
-
-
 const badConfig = {
-  imageOutputPath: 'test/output/test1.png',
-  hocrPath: 'test/hocr/test1.hocr'
+  pdfInputPath: path.join(__dirname, './pdf'),
+  directoryOutputPath: path.join(__dirname, './tmp/images'),
+  directoryPartialPath: path.join(__dirname, './tmp/images'),
+  tmp: path.join(__dirname, "./tmp"),
+  debug:true
 }
-
 describe('Dependances', () => {
   it('devrait avoir accès à la commande tesseract sur cette machine', (done) => {
     exec('which tesseract', (err, stdout, stderr) => {
@@ -26,13 +31,27 @@ describe('Dependances', () => {
       done()
     })
   })
+  it('devrait avoir accès à la commande ghostscript sur cette machine', (done) => {
+    exec('which ghostscript', (err, stdout, stderr) => {
+      void expect(err, 'la commande which devrait exister').to.be.null;
+      void expect(stderr, 'la commande which ne doit pas retourner d`erreur').to.be.empty;
+      void expect(stdout, "ghostscript introuvable").to.be.not.null;
+      done()
+    })
+  })
+  it('devrait avoir accès à opencv sur cette machine', (done) => {
+    const exist = fs.existsSync('/usr/local/include/opencv')
+    void expect(exist, "opencv introuvable").to.be.true;
+    done();
+  })
 })
+
 
 
 
 describe('Initialisation', () => {
   it("devrait renvoyer une erreur avec un parametre manquant", (done) => {
-    Coincides.init(badConfig).then(_ => {
+    new pfe(badConfig).then(_ => {
       assert.isOk(false, 'devrait soulever une erreur')
       done()
     }).catch(err => {
@@ -42,10 +61,11 @@ describe('Initialisation', () => {
   })
 
   it("ne devrait pas renvoyer une erreur avec de bons parametres", (done) => {
-    Coincides.init(goodConfig).then(_ => {
+    new pfe(goodConfig).then(_ => {
       assert.isOk(true, 'ne devrait pas instancier la classe')
       done()
     }).catch(err => {
+      console.log("lekjzefklzef")
       console.error('error', err);
       assert.isOk(false, 'ne devrait pas soulever une erreur')
       done()
@@ -53,8 +73,16 @@ describe('Initialisation', () => {
   })
 
   it("devrait lancer l'ocerisation", (done) => {
-    assert.isOk(fs.existsSync("test/hocr/test1.hocr"), "pas d'hocr")
-    done()
+    new pfe(goodConfig).then(self=>{
+      return self.exec()
+    }).then(partials=>{
+      console.log(partials)
+      assert.isOk(false, 'devrait contenir des partials')
+      done()
+    }).catch(err=>{
+      assert.isOk(false, 'devrait lancer la detection')
+      done()
+    })
   })
 
 })
